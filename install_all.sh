59b19f0e8b44bd89d026d90bd0b9699e96324f48
George <7jagjag@gmail.com>
2016-05-12 23:09:05 -0600

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ### Setup
 
-echo "Installing Pathogen...";
+echo -e "\nInstalling Pathogen...";
 mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim;
 
@@ -11,13 +11,15 @@ git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim;
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline;
 curl -LSso ~/.vim/colors/PaperColor.vim https://raw.githubusercontent.com/NLKNguyen/papercolor-theme/master/colors/PaperColor.vim;
 
-if [[ -f ~/.vimrc ]]; then
-  DATE=`date +%Y-%m-%d:%H:%M:%S`;
-  echo "::::::::::: WARNING :::::::::::::::::::"
-  echo "You had an existing vimrc, creating backup: $DATE"
-  echo ":::::::::::::::::::::::::::::::::::::::"
-  cp ~/.vimrc ~/.vimrc_copy_$DATE;
-fi
-
 echo "Creating ~/.vimrc with default config"
-curl -LSso ~/.vimrc https://raw.githubusercontent.com/g3org3/custom-vim/master/vimrc
+curl -LSso ~/.vimrc_new https://raw.githubusercontent.com/g3org3/custom-vim/master/vimrc
+if [[ -f ~/.vimrc ]]; then
+  DIFF=`diff ~/.vimrc ~/.vimrc_new`;
+  if [[ -n "$DIFF" ]]; then
+    DATE=`date +%Y-%m-%d:%H:%M:%S`;
+    echo "::::::::::: WARNING :::::::::::::::::::"
+    echo "You had an existing vimrc, creating backup: $DATE"
+    echo -e ":::::::::::::::::::::::::::::::::::::::\n"
+    cp ~/.vimrc ~/.vimrc_copy_$DATE;
+  fi
+fi
