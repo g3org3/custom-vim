b92add5add83ea386413dcc882284c119ec5982c
George <7jagjag@gmail.com>
2016-05-12 22:59:33 -0600

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -11,5 +11,13 @@ git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim;
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline;
 curl -LSso ~/.vim/colors/PaperColor.vim https://raw.githubusercontent.com/NLKNguyen/papercolor-theme/master/colors/PaperColor.vim;
 
+if [[ -f ~/.vimrc ]]; then
+  DATE=`date +%Y-%m-%d:%H:%M:%S`;
+  echo "::::::::::: WARNING :::::::::::::::::::"
+  echo "You had an existing vimrc, creating backup: $DATE"
+  echo ":::::::::::::::::::::::::::::::::::::::"
+  cp ~/.vimrc ~/.vimrc_copy_$DATE;
+fi
+
 echo "Creating ~/.vimrc with default config"
 curl -LSso ~/.vimrc https://raw.githubusercontent.com/g3org3/custom-vim/master/vimrc
