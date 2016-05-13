7a08581425750fcffdb5c1720e11da7f7fdbe4c7
George <7jagjag@gmail.com>
2016-05-12 23:12:22 -0600

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -5,10 +5,13 @@ echo -e "\nInstalling Pathogen...";
 mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim;
 
-echo "Intalling Dependencies";
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree;
-git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim;
-git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline;
+echo "Installing Dependencies";
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree && vim -u NONE -c "helptags nerdtree/doc" -c q;
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim && vim -u NONE -c "helptags ctrlp.vim/doc" -c q;
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline && vim -u NONE -c "helptags vim-airline/doc" -c q;
+git clone https://github.com/tpope/vim-fugitive ~/.vim/bundle/vim-fugitive && vim -u NONE -c "helptags vim-fugitive/doc" -c q;
+
+echo "Installing Themes"
 curl -LSso ~/.vim/colors/PaperColor.vim https://raw.githubusercontent.com/NLKNguyen/papercolor-theme/master/colors/PaperColor.vim;
 
 echo "Creating ~/.vimrc with default config"
