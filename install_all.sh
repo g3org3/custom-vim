bd1c425519f524682a1078cb945f0d9ea12d84a9
Jorge Adolfo <7jagjag@gmail.com>
2021-12-10 18:27:25 +0100

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -11,6 +11,7 @@ git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim;
 git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/vim-airline;
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive;
 git clone https://github.com/mattn/emmet-vim.git ~/.vim/bundle/emmet-vim;
+git clone https://github.com/mhartington/oceanic-next.git ~/.vim/bundle/oceanic-next;
 git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter;
 git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/bundle/editorconfig-vim;
 git clone https://github.com/powerline/fonts.git ~/.vim/bundle/powerline-fonts;
