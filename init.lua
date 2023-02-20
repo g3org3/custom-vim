64a2226fea8dc20c96f1e1855c2493717f61eb53
Jorge Adolfo <7jagjag@gmail.com>
2023-02-20 20:38:52 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -75,6 +75,7 @@ require('lazy').setup({
 
   'neoclide/coc.nvim',
   'kessejones/git-blame-line.nvim',
+  'nvim-tree/nvim-web-devicons',
   {
     'nvim-tree/nvim-tree.lua',
     requires = {
@@ -539,8 +540,40 @@ vim.filetype.add({
 
 vim.g.astro_stylus = 'enable'
 vim.g.astro_typescript = 'enable'
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+local keyset = vim.keymap.set
+-- Autocomplete
+function _G.check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+
+-- Make <CR> to accept selected completion item or notify coc.nvim to format
+-- <C-g>u breaks current undo, please make your own choice
+keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+keyset("n", "gtd", "<Plug>(coc-definition)", {silent = true})
+keyset("n", "gtr", "<Plug>(coc-references)", {silent = true})
+
+
+-- Use K to show documentation in preview window
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+
 
 -- require'lspconfig'.astro.setup{}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
